71e15c0316230c9e4868024ef2c3e762a67cf838
Adam Bretz <arbretz@gmail.com>
2015-07-15 17:05:19 -0400

    PR updates 1.

--- client/app.js
+++ client/app.js
@@ -1,11 +1,12 @@
 const angular = require('angular');
 const templates = require('./templates');
 
-angular.module('App', [require('angular-route'), 'List'])
-.config(['$routeProvider', function ($routeProvider) {
+const App = angular.module('App', [require('angular-route'), 'List']);
+
+App.config(['$routeProvider', function ($routeProvider) {
   $routeProvider.when('/', {
     template: templates.list,
     controller: 'ListController',
     controllerAs: 'list'
-  })
+  });
 }]);
